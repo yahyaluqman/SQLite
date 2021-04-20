7686700f49f1e0b9838416e46fc6491c08c75483
yahyaluqman <yahya.luqmanu21@gmail.com>
2021-04-20 23:01:25 +0700

    Initial commit

---
+++ app/src/main/java/com/example/kelasasqlite/adapter/TemanAdapter.java
@@ -0,0 +1,64 @@
+package com.example.kelasasqlite.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.kelasasqlite.R;
+import com.example.kelasasqlite.database.Teman;
+
+import java.util.ArrayList;
+
+public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHolder> {
+    private ArrayList<Teman> listData;
+
+    public TemanAdapter(ArrayList<Teman> listData) {
+        this.listData = listData;
+    }
+
+    @Override
+    public TemanViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInf = LayoutInflater.from(parent.getContext());
+        View view = layoutInf.inflate(R.layout.row_data_teman,parent, false);
+        return new TemanViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TemanViewHolder holder, int position) {
+    String nm,tlp;
+
+    nm = listData.get(position).getNama();
+    tlp = listData.get(position).getTelpon();
+
+    holder.namaTxt.setText(nm);
+    holder.telponTxt.setText(tlp);
+    }
+
+    @Override
+    public int getItemCount() {
+        return (listData != null)?listData.size() : 0;
+    }
+
+    public class TemanViewHolder extends RecyclerView.ViewHolder {
+        private CardView cardku;
+        private TextView namaTxt,telponTxt;
+        public TemanViewHolder(View view) {
+            super(view);
+            cardku = (CardView) view.findViewById(R.id.kartuku);
+                    namaTxt = (TextView) view.findViewById(R.id.textNama);
+                    telponTxt = (TextView) view.findViewById(R.id.textTelpon);
+
+                    cardku.setOnLongClickListener(new View.OnLongClickListener() {
+                        @Override
+                        public boolean onLongClick(View v) {
+                            return true;
+                        }
+                    });
+        }
+    }
+}
